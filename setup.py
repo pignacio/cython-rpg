cb8e0ee0f5575f5ce01492882d78ec0ae695f6e4
Ignacio Rossi <rossi.ignacio@gmail.com>
2016-06-08 11:54:57 -0300

    setup.py: compile with profiler directives

--- setup.py
+++ setup.py
@@ -2,15 +2,25 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division
 
+import os
+
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
-import os
 
 os.environ['CC'] = 'clang-3.5'
 
 CYTHON_EXTENSIONS = (
-    Extension('*', ["rpg/**/*.pyx"], libraries=['SDL2', 'SDL2_image']),
-)
+    Extension('*',
+              ["rpg/**/*.pyx"],
+              include_dirs=['rpg'],
+              libraries=['SDL2', 'SDL2_image'],
+#               define_macros=[('CYTHON_WITHOUT_ASSERTIONS', 1)],
+              extra_compile_args=['-Wno-unused-function',
+                                  '-Wno-incompatible-pointer-types',
+                                 ]), )
 
-setup(ext_modules=cythonize(CYTHON_EXTENSIONS))
+setup(ext_modules=cythonize(
+                            CYTHON_EXTENSIONS,
+                            compiler_directives={'profile': True},
+))
